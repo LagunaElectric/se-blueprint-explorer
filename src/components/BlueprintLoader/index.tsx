b1eaeb6edfd0c7aaaa99d3da2eacc3aa8353864c
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2022-10-09 21:49:29 -0400

    The answer was functional components all along

--- src/components/BlueprintLoader/index.tsx
+++ src/components/BlueprintLoader/index.tsx
@@ -1,40 +1,27 @@
-import React, { Component } from 'react'
-
-type Props = {}
-
-type State = {
-  files?: FileList | null,
-  fileContents?: String | ArrayBuffer | null
-}
-
-export default class BlueprintLoader extends Component<Props, State> {
-  state: State = { files: null }
-  inputRef: React.RefObject<HTMLInputElement>
-
-  constructor(props: Props) {
-    super(props)
-    this.inputRef = React.createRef<HTMLInputElement>()
-    this.onChange = this.onChange.bind(this)
-  }
+import React, { useState } from 'react'
 
 
-  onChange(event: React.ChangeEvent<HTMLInputElement>) {
+export default function BlueprintLoader() {
+  const [fileContent, setFileContent] = useState('')
+
+
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     var file = event.target.files?.[0] ?? new Blob()
     var reader = new FileReader()
     reader.onload = function (event) {
       // The file's text will be printed here
       console.log(event.target?.result)
+      setFileContent(String(event.target?.result))
     }
     reader.readAsText(file)
   }
 
-  render() {
-    return (
-      <>
-        <input type="file" ref={ this.inputRef } onChange={ this.onChange } />
-        <button onClick={ (e: React.MouseEvent<HTMLButtonElement>) => console.log(this.state) }>log bp</button>
-        <p>{ this.state.fileContents?.toString() }</p>
-      </>
-    )
-  }
+  return (
+    <>
+      <input type="file" onChange={ onChange } />
+      <button onClick={ (e: React.MouseEvent<HTMLButtonElement>) => console.log(fileContent) }>log bp</button>
+      <p>{ fileContent.substring(0, 255) }</p>
+    </>
+  )
 }
